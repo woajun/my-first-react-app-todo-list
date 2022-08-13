327c7d779656e626c64fa8e82f78881acaafdff4
juni <woajun@naver.com>
2022-08-13 17:15:12 +0900

    remove todo

--- src/components/TodoBox.js
+++ src/components/TodoBox.js
@@ -8,6 +8,7 @@ class TodoBox extends React.Component {
     super(props);
     this.state = { todos: [] };
     this.addTodo = this.addTodo.bind(this);
+    this.removeTodo = this.removeTodo.bind(this);
   }
   addTodo(aTodo) {
     const todo = {
@@ -18,12 +19,21 @@ class TodoBox extends React.Component {
     };
     this.setState({ todos: [...this.state.todos, todo] });
   }
+  removeTodo(key) {
+    const filtered = this.state.todos.filter((e) => e.key !== key);
+    this.setState({ todos: filtered });
+  }
   getKey() {
     return this.key++;
   }
   render() {
     const todos = this.state.todos.map((e) => (
-      <TodoItem key={e.key} content={e.content} />
+      <TodoItem
+        key={e.key}
+        content={e.content}
+        onDeleteEvent={this.removeTodo}
+        id={e.key}
+      />
     ));
     return (
       <div className="mt-7 bg-white  rounded-xl shadow-lg dark:bg-gray-800 dark:border-gray-700">
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,13 +1,24 @@
 import React from "react";
 
 class TodoItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+  handleDelete() {
+    console.log(this.props.id);
+    this.props.onDeleteEvent(this.props.id);
+  }
   render() {
     return (
       <div className="mt-2 py-3 px-4 block w-full border-2 border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 shadow-sm">
         <label>{this.props.content}</label>
         <span className="float-right">
           <button>완료</button>
-          <button className="ml-3">삭제</button>
+          <button className="ml-3" onClick={this.handleDelete}>
+            삭제
+          </button>
         </span>
       </div>
     );
